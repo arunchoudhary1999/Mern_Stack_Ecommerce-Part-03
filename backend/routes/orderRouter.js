471404aa96710e0fdc82547175e2382169dd091a
Arun Choudhary 1999 <74254094+arunchoudhary1999@users.noreply.github.com>
2021-11-30 10:49:26 +0530

    Update orderRouter.js

--- backend/routes/orderRouter.js
+++ backend/routes/orderRouter.js
@@ -18,17 +18,10 @@ router.route("/order/:id").get(isAuthenticatedUser, getSingleOrder);
 router.route("/orders/me").get(isAuthenticatedUser, myOrders);
 
 router.route("/admin/orders").get(isAuthenticatedUser, authorizeRoles("admin"), getAllOrders);
-// isko keval admin he ker sakta hai is liye hmne yaha deya hai -> authorizeRoles("admin")
-// or yaha per hmne getAllOrders function ka route set ker deya hai, get method ka use ker ke, 
-// or isko import bhi ker leya hai
 
 router
     .route("/admin/order/:id")
     .put(isAuthenticatedUser, authorizeRoles("admin"), updateOrder)
     .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteOrder);
-// isme bhi keval admin he ker sakta hai is liye hmne yaha deya hai -> authorizeRoles("admin") dono me, or dono he
-// same route me rahegay keoke product id se he kise order ko update or delete keya ja sakta hai, is liye hmne dono
-// ko same route me leya hai
-// dono function ( updateOrder, deleteOrder ko ka route set ker hm ne import ker leya hai )
 
 module.exports = router;
